9f0fe2b558ba69931d4957d75908cd1add417bce
vishal <vishalkumarvisu888@gmail.com>
2023-12-21 09:03:18 +0530

    Starting the server and set the db connection.

---
+++ index.js
@@ -0,0 +1,2 @@
+const server                       = require("./server");
+server.startServer();              // just trigaring point of the server file
---
+++ server.js
@@ -0,0 +1,34 @@
+const express                     = require("express");
+const mongoose                    = require("mongoose");
+require("dotenv").config();       // configuring env file
+
+
+const app                         = express();
+const port                        = process.env.APP_PORT || 8000;
+
+
+module.exports.startServer        = async () =>
+{
+    try {
+        await mongoose.connect(process.env.DB_URL)
+        .then(() =>
+        {
+           app.listen(port,  (err) => {
+            if(err)
+            {
+                console.log(`Error while starting the server :: ${err}`);
+            } 
+            else
+            {
+              console.log(`Server is up and running or port :: ${port}`);
+            }
+           })
+        })
+        .then(() =>
+        {
+           console.log(`Connected successfuly with DB :: MongoDB`);
+        })
+    } catch (error) {
+        console.log(`Error in starting the server :: ${error}`);
+    }
+}
